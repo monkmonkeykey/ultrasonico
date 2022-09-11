3faaf544295b4f20e01ff21715f1f849547998c5
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2022-09-11 00:33:22 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(distancia)
+    music.setVolume(0)
 })
-let distancia = 0
-distancia = 0
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(255)
+})
 music.setVolume(127)
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P1, DigitalPin.P0)
 basic.forever(function () {
-    if (distancia <= 10) {
+    led.plotBarGraph(
+    makerbit.getUltrasonicDistance(DistanceUnit.CM),
+    100
+    )
+    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 10) {
+        music.playTone(784, music.beat(BeatFraction.Sixteenth))
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 11 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 50) {
+        music.playTone(659, music.beat(BeatFraction.Eighth))
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 51 && makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 100) {
+        music.playTone(523, music.beat(BeatFraction.Quarter))
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 101) {
         music.playTone(262, music.beat(BeatFraction.Whole))
-    } else if (distancia >= 11 && distancia <= 50) {
-        music.playTone(262, music.beat(BeatFraction.Half))
-    } else if (distancia >= 51 && distancia >= 100) {
-        music.playTone(262, music.beat(BeatFraction.Quarter))
     } else {
     	
     }
-    led.plotBarGraph(
-    distancia,
-    300
-    )
 })
