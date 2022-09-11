d55350a8c53bf75eab4a0cdbcc2ac4a72e0fccd9
Josué Martínez <30963079+monkmonkeykey@users.noreply.github.com>
2022-09-10 23:59:56 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showNumber(distancia)
 })
 let distancia = 0
+distancia = 0
+music.setVolume(127)
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P1, DigitalPin.P0)
-control.inBackground(function () {
+basic.forever(function () {
     if (distancia <= 10) {
-    	
+        music.playTone(262, music.beat(BeatFraction.Whole))
     } else if (distancia >= 11 && distancia <= 50) {
-    	
+        music.playTone(262, music.beat(BeatFraction.Half))
     } else if (distancia >= 51 && distancia >= 100) {
-    	
+        music.playTone(262, music.beat(BeatFraction.Quarter))
     } else {
     	
     }
+    led.plotBarGraph(
+    distancia,
+    300
+    )
 })
